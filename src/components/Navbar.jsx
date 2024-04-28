6f8dc6c963b6aaae5ad2069700efa30be51d19a6
nwj002 <nwj.shrestha@gmail.com>
2024-04-28 13:45:35 +0545

    regestration logic.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">underage <span className='text-danger'>booze</span></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
+        <>
+            <div className='container'>
+                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="#">Rakshi <span className='text-danger'>Pasal</span></a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Link</a>
+                                </li>
 
-                    </ul>
-                    <form className="d-flex" role="search">
-                        <button className="btn btn-success" type="submit">Login</button>
-                        <button className="btn btn-primary ms-2" type="submit">register</button>
-                    </form>
-                </div>
+                            </ul>
+                            <form className="d-flex" role="search">
+                                <Link to={'/Login'} className="btn btn-success" type="submit">Login</Link>
+                                <Link to={'/Register'} className="btn btn-primary ms-2" type="submit">register</Link>
+                            </form>
+                        </div>
+                    </div>
+                </nav>
+
             </div>
-        </nav>
+        </>
     )
 }
 
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,11 +1,77 @@
-import React from "react"; 
+import React, { useState } from "react";
 
-const Register = () => { 
+const Register = () => {
+
+    //login section
+
+    //make a usestate for 5 fields
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmpassword, setConfirmPassword] = useState('')
+
+    //use state for error message
+    const [firstNameError, setFirstNameError] = useState('')
+    const [lasttNameError, setLasttNameError] = useState('')
+    const [emailError, setEmailError] = useState('')
+    const [passwordError, setPasswordError] = useState('')
+    const [confirmpasswordError, setConfirmPasswordError] = useState('')
+
+
+    // make a each function for changing the value
+    const handleFirstname = (e) => {
+        setFirstName(e.target.value);
+    }
+    const handleLastname = (e) => {
+        setLastName(e.target.value);
+    }
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+    const handleComfirmPassword = (e) => {
+        setConfirmPassword(e.target.value);
+    }
+    //submit button function
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(firstName, lastName, email, password, confirmpassword)
+    }
+
     return (
-        <div>
-            Register!
-        </div>
+        <>
+            <div className="container mt-2">
+                <h1> create an account</h1>
+                <form className="  w-50">
+                    <label>FirstName :{firstName}</label>
+                    <input onChange={handleFirstname} type="text" className="form-control" placeholder="enter your first name" />
+
+                    <label className="mt-2">Lastname :{lastName} </label>
+                    <input onChange={handleLastname} type="text" className="form-control" placeholder="enter your last name" />
+
+                    <label className="mt-2">email :{email}</label>
+                    <input onChange={handleEmail} type="text" className="form-control" placeholder="enter your email" />
+
+                    <label className="mt-2">Password :{password}</label>
+                    <input onChange={handlePassword} type="text" className="form-control" placeholder="enter your password" />
+
+                    <label className="mt-2">Comfirm Password :{confirmpassword}</label>
+                    <input onChange={handleComfirmPassword} type="text" className="form-control" placeholder="enter your comfirm password" />
+
+                    <button onClick={handleSubmit} className="btn btn-dark mt-3 w-100">create an account</button>
+                </form>
+
+            </div>
+
+        </>
     )
 }
 
-export default Register; 
+export default Register;
+
+//step 1: make a complete ui of regestration page(fields, button, etc)
+//step 2: input - make a state
+//step 3: on change - set the value to the state.
